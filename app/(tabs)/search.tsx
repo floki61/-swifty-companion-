8245e04054373df92de35fec5618aad723ceea46
floki61 <flo61ki@gmail.com>
2025-05-26 22:49:35 +0100

    fix searchquery state by adding lastsearchquery and added a timeout for loading state

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -7,14 +7,18 @@ import { ActivityIndicator, ScrollView, Text, View } from "react-native";
 
 export default function SearchScreen() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [lastSearchedQuery, setLastSearchedQuery] = useState("");
   const { searchUser, userData, error } = useUserSearch();
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = async () => {
     if (searchQuery.trim()) {
       setIsLoading(true);
-      await searchUser(searchQuery.trim().toLowerCase());
-      setIsLoading(false);
+      setTimeout(async () => {
+        setLastSearchedQuery(searchQuery.trim().toLowerCase());
+        await searchUser(searchQuery.trim().toLowerCase());
+        setIsLoading(false);
+      }, 500);
     }
   };
 
@@ -22,19 +26,21 @@ export default function SearchScreen() {
     <View className="flex-1 bg-black px-4">
       <SearchInput searchQuery={searchQuery} setSearchQuery={setSearchQuery} handleSearch={handleSearch} error={error} />
       <ScrollView className="flex-1 pb-20" showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
-        {isLoading ? (
-          <View className="flex-1 justify-center items-center">
+        {
+        isLoading ? (
+          <View className="flex-1 justify-center items-center pt-36">
             <ActivityIndicator size="large" color="#10b981" />
             <Text className="text-green-500 mt-4">Searching...</Text>
           </View>
-        ) : userData ? (
+        ) : 
+        userData ? (
           <View className="flex-1">
             <UserProfile user={userData} />
           </View>
         ) : error ? (
           <View className="flex-1 items-center pt-36">
             <IconSymbol name="exclamationmark.circle" size={60} color="#ef4444" />
-            <Text className="text-white text-lg mt-4 text-center">No user found with the name "{searchQuery}"</Text>
+            <Text className="text-white text-lg mt-4 text-center">No user found with the name {lastSearchedQuery}</Text>
             <Text className="text-gray-400 text-center mt-2">Try searching with a different username</Text>
           </View>
         ) : (
