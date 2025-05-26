ab75aa348f340b7cbab50fe47e8d0270bceed714
floki61 <flo61ki@gmail.com>
2025-05-26 22:48:29 +0100

    removed some unused compnents and added .env file

--- config/api.ts
+++ config/api.ts
@@ -1,16 +1,16 @@
 export const INTRA_API_CONFIG = {
-  CLIENT_ID: "u-s4t2ud-32d0493a98ffbdf528ddb646cf737df859c7c84ef53ff7ee52aa8df4c0b014be",
-  CLIENT_SECRET: "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",
+  CLIENT_ID: process.env.EXPO_PUBLIC_CLIENT_ID,
+  CLIENT_SECRET: process.env.EXPO_PUBLIC_CLIENT_SECRET,
 
   // Mobile configuration
   MOBILE_REDIRECT_URI: "swiftycompanion://(tabs)",
-  MOBILE_AUTH_URL:"https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-32d0493a98ffbdf528ddb646cf737df859c7c84ef53ff7ee52aa8df4c0b014be&redirect_uri=swiftycompanion%3A%2F%2F%28tabs%29&response_type=code",
+  MOBILE_AUTH_URL: process.env.EXPO_PUBLIC_MOBILE_AUTH_URL,
 
   // Web configuration
-  WEB_REDIRECT_URI: "http://localhost:8081/(tabs)",
-  WEB_AUTH_URL:"https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-32d0493a98ffbdf528ddb646cf737df859c7c84ef53ff7ee52aa8df4c0b014be&redirect_uri=http%3A%2F%2Flocalhost%3A8081%2F%28tabs%29&response_type=code",
+  WEB_REDIRECT_URI: process.env.EXPO_PUBLIC_WEB_REDIRECT_URI,
+  WEB_AUTH_URL: process.env.EXPO_PUBLIC_WEB_AUTH_URL,
 
   // API endpoints
-  TOKEN_ENDPOINT: "https://api.intra.42.fr/oauth/token",
-  API_BASE_URL: "https://api.intra.42.fr/v2",
+  TOKEN_ENDPOINT: process.env.EXPO_PUBLIC_TOKEN_ENDPOINT,
+  API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
 };
